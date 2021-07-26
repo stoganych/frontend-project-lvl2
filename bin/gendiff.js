edbc1a4ac15386294ae882bb8df02f55863b59fd
Timofey Ludvenko <stoganych@gmail.com>
2021-07-26 21:02:29 +0300

    add new option -f that chose format of file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 import {
   Command
 } from 'commander';
@@ -6,6 +7,8 @@ const program = new Command();
 
 program.version('0.0.1');
 program.description('Compares two configuration files and shows a difference.');
+program.option('-f, --format [type]', 'output format');
+program.argument('<filepath1>')
+  .argument('<filepath2>');
 
-program.parse();
-
+program.parse(process.argv);

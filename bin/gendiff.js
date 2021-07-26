72ce3d283fc71d906572336b151e0ddaf3705915
Timofey Ludvenko <stoganych@gmail.com>
2021-07-26 20:10:21 +0300

    fix gendiff and add babel

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,9 +6,6 @@ const program = new Command();
 
 program.version('0.0.1');
 program.description('Compares two configuration files and shows a difference.');
-program
-  .option('-V, --version', 'output the version number')
-  .option('-h, --help', 'output usage information');
 
 program.parse();
 

5940476b0ec51f615cbbc1c00d82f0bb1ed98e35
Timofey Ludvenko <stoganych@gmail.com>
2021-07-26 08:19:05 +0300

    add startup file getdiff with comander

---
+++ bin/gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import {
+  Command
+} from 'commander';
+const program = new Command();
+
+program.version('0.0.1');
+program.description('Compares two configuration files and shows a difference.');
+program
+  .option('-V, --version', 'output the version number')
+  .option('-h, --help', 'output usage information');
+
+program.parse();
+

024948657df178cccc48a2a20db755da91fe464d
Timofey Ludvenko <stoganych@gmail.com>
2021-07-29 21:16:11 +0300

    fix functions

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,14 +3,14 @@
 import {
   Command,
 } from 'commander';
-import compareFiles from '../src/index.js';
+import * as diff from '../src/index.js';
 
 const program = new Command();
 
 program.version('0.0.1');
 program.argument('<filepath1>')
   .argument('<filepath2>')
-  .action((path1, path2) => compareFiles(path1, path2));
+  .action((path1, path2) => diff.compareFiles(path1, path2));
 program.description('Compares two configuration files and shows a difference.');
 program.option('-f, --format [type]', 'output format');
 

daf07b62f65797e984be35da79a75bbd89b878a3
Timofey Ludvenko <stoganych@gmail.com>
2021-07-30 12:37:49 +0300

    update func

--- src/index.js
+++ src/index.js
@@ -1,27 +1,3 @@
-// import * as fs from 'fs';
-// import _ from 'lodash';
-
-// const getFile = (pathToFile) => fs.readFileSync(pathToFile);
-// const parseFile = (file) => JSON.parse(getFile(file));
-// const printStr = (str) => console.log(`{${str}\n}`);
-
-// const compareFiles = (pathToFile1, pathToFile2) => {
-//   const obj1 = parseFile(pathToFile1);
-//   const obj2 = parseFile(pathToFile2);
-//   const keys = _.union(Object.keys(obj1), Object.keys(obj2))
-//     .sort()
-//     .reduce((acc, key) => {
-//       if (!_.has(obj1, key)) return `${acc}\n  + ${key}: ${obj2[key]}`;
-//       if (!_.has(obj2, key)) return `${acc}\n  - ${key}: ${obj1[key]}`;
-//       if (obj1[key] !== obj2[key]) return `${acc}\n  - ${key}: ${obj1[key]}
-//              \n  + ${key}: ${obj2[key]}`;
-//       return `${acc}\n    ${key}: ${obj1[key]}`;
-//     }, '');
-//   return printStr(keys);
-// };
-
-// export default compareFiles;
-
 import { readFileSync } from 'fs';
 import _ from 'lodash';
 
@@ -43,6 +19,5 @@ export const compareFiles = (path1, path2) => {
   const keys = _.union(Object.keys(obj1), Object.keys(obj2))
     .sort()
     .reduce((diffString, key) => `${diffString}\n  ${generateDiffLine(obj1, obj2, key)}`, '');
-  console.log(`{${keys}\n}`);
-  return keys;
+  return console.log(`{${keys}\n}`);
 };

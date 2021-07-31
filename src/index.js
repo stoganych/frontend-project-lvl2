cc6e325371da17212a47d11977bed5888a19b111
Timofey Ludvenko <stoganych@gmail.com>
2021-07-31 11:26:55 +0300

    change imports

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-import { readFileSync } from 'fs';
+import path from 'path';
 import _ from 'lodash';
+import stylish from './stylish.js';
+import parser from './parsers.js';
 
-const getFile = (path) => readFileSync(path);
-const parseJSONFromFile = (path) => JSON.parse(getFile(path));
+const getFormat = (configPath) => path.extname(configPath);
 
-const stringifyKeyValue = (object, key) => `${key}: ${object[key]}`;
-
-export const generateDiffLine = (object1, object2, key) => {
-  if (!_.has(object1, key)) return `+ ${stringifyKeyValue(object2, key)}`;
-  if (!_.has(object2, key)) return `- ${stringifyKeyValue(object1, key)}`;
-  if (object1[key] !== object2[key]) return `- ${stringifyKeyValue(object1, key)}\n  + ${stringifyKeyValue(object2, key)}`;
-  return `  ${stringifyKeyValue(object1, key)}`;
-};
-
-export const compareFiles = (path1, path2) => {
-  const obj1 = parseJSONFromFile(path1);
-  const obj2 = parseJSONFromFile(path2);
+const compareFiles = (path1, path2) => {
+  const obj1 = parser(path1, getFormat(path1));
+  const obj2 = parser(path2, getFormat(path1));
   const keys = _.union(Object.keys(obj1), Object.keys(obj2))
     .sort()
-    .reduce((diffString, key) => `${diffString}\n  ${generateDiffLine(obj1, obj2, key)}`, '');
+    .reduce((diffString, key) => `${diffString}\n  ${stylish(obj1, obj2, key)}`, '');
   return console.log(`{${keys}\n}`);
 };
+
+export default compareFiles;

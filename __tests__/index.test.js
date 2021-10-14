bb06043477d7dd1e4c4511b1c986bdf113e80460
Timofey Ludvenko <stoganych@gmail.com>
2021-10-14 14:26:00 +0300

    add txt fixtures for test of json and yml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,75 +1,34 @@
+import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
-import path from 'path';
-import { jest } from '@jest/globals';
-import compareFiles from '../src/index.js';
+import path, { dirname } from 'path';
+import genDiff from '../src/index.js';
 
-global.console = {
-  log: jest.fn(),
-  info: jest.fn(),
-  error: jest.fn(),
-};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-test('gendiff', async () => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
+test('json', () => {
   const file1 = getFixturePath('file1.json');
   const file2 = getFixturePath('file2.json');
+  const result = readFile('result.txt');
 
-  const tryAnswer = `{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}`;
+  expect(genDiff(file1, file2)).toBe(result);
+});
 
-  compareFiles(file1, file2);
+test('yaml', () => {
+  const file1 = getFixturePath('file1.yml');
+  const file2 = getFixturePath('file2.yml');
+  const result = readFile('resultyml.txt');
 
-  expect(global.console.log).toHaveBeenCalledWith(tryAnswer);
+  expect(genDiff(file1, file2)).toBe(result);
+});
 
-  const fileYaml1 = getFixturePath('fileYaml1.yml');
-  const fileYaml2 = getFixturePath('fileYaml2.yaml');
+test('plain', () => {
+  const file1 = getFixturePath('file1.json');
+  const file2 = getFixturePath('file2.json');
+  const result = readFile('resultplain.txt');
 
-  compareFiles(fileYaml1, fileYaml2);
-
-  expect(global.console.log).toHaveBeenCalledWith(tryAnswer);
+  expect(genDiff(file1, file2, 'plain')).toBe(result);
 });

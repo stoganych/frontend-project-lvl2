fc2523c414f04a8a3d800ebd5f909062a7315d7f
Timofey Ludvenko <stoganych@gmail.com>
2021-10-14 14:06:25 +0300

    add func of create abstract tree

--- src/index.js
+++ src/index.js
@@ -1,45 +1,32 @@
-import {
-  readFileSync,
-} from 'fs';
-import path from 'path';
 import _ from 'lodash';
-import stylish from './stylish.js';
 import parser from './parsers.js';
+import format from './formatters/index.js';
 
-const getFile = (pathToFile) => readFileSync(pathToFile);
-const getFormat = (configPath) => path.extname(configPath);
-
-const createATS = (tree1, tree2) => {
-  const createObj = (name, depth, type, children) => ({
-    name,
-    depth,
-    type,
-    children,
+const createTree = (obj1, obj2) => {
+  const keys = Object.keys({ ...obj1, ...obj2 });
+  const sortedKeys = _.sortBy(keys);
+  return sortedKeys.map((key) => {
+    if (!_.has(obj1, key)) {
+      return ['add', { key, val: obj2[key] }];
+    }
+    if (!_.has(obj2, key)) {
+      return ['remove', { key, val: obj1[key] }];
+    }
+    if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
+      return ['recursion', { key, val: createTree(obj1[key], obj2[key]) }];
+    }
+    if (_.isEqual(obj1[key], obj2[key])) {
+      return ['same', { key, val: obj1[key] }];
+    }
+    return ['updated', { key, val1: obj1[key], val2: obj2[key] }];
   });
-
-  const iter = (node1, node2, depth) => {
-    const keys = _.union(Object.keys(node1), Object.keys(node2)).sort();
-    return keys.map((key) => {
-      if (!_.isObject(node1[key]) || !_.isObject(node2[key])) {
-        if (!_.has(node1, key)) return createObj(key, depth, 'plus', node2[key]);
-        if (!_.has(node2, key)) return createObj(key, depth, 'minus', node1[key]);
-        if (node1[key] !== node2[key]) return [createObj(key, depth, 'minus', node2[key]), createObj(key, depth, 'plus', node1[key])];
-        return createObj(key, depth, 'equal', node1[key]);
-      }
-      if (!_.has(node1, key)) return createObj(key, depth, 'minus', iter(node1[key], node2[key], depth + 2));
-      if (!_.has(node2, key)) return createObj(key, depth, 'plus', iter(node1[key], node2[key], depth + 2));
-      return createObj(key, depth, 'equal', iter(node1[key], node2[key], depth + 2));
-    });
-  };
-  return iter(tree1, tree2, 0);
 };
 
-const compareFiles = (path1, path2) => {
-  const obj1 = parser(getFile(path1), getFormat(path1));
-  const obj2 = parser(getFile(path2), getFormat(path2));
-  const ATS = createATS(obj1, obj2);
-
-  console.log(ATS);
+const genDiff = (filepath1, filepath2, formatType = 'stylish') => {
+  const file1 = parser(filepath1);
+  const file2 = parser(filepath2);
+  const tree = createTree(file1, file2);
+  console.log(JSON.stringify(tree));
+  return format(tree, formatType);
 };
-
-export default compareFiles;
+export default genDiff;
